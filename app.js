14f9b6a16b4332b5ec5e189b8c03bc5887d60438
Keenan (Ezra) Austin <ezra@Keenans-MacBook-Pro.local>
2021-06-16 12:55:33 +0300

    added db seed folder

--- app.js
+++ app.js
@@ -1,7 +1,21 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const app = express();
 const port = 3000;
 const path = require('path');
+const Campground = require('./models/campground');
+
+mongoose.connect('mongodb://localhost:/yelpCamp', {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true
+});
+
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error"));
+db.once("open", () => {
+    console.log("Database Connected");
+});
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
@@ -10,6 +24,12 @@ app.get('/', (req, res) => {
     res.render('home')
 })
 
+app.get('/makecampground', async (req, res) => {
+    let camp = new Campground({title: "My Backyard"})
+    await camp.save();
+    res.send(camp);
+})
+
 app.listen(port, () => {
-    console.log(`Listening on port ${port}`);
+    console.log(`Listening on Port ${port}`);
 });
---
+++ seeds/seeds.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose');
+const cities = require('./cities');
+const Campground = require('../models/campground');
+
+mongoose.connect('mongodb://localhost:/yelpCamp', {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true
+});
+
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error"));
+db.once("open", () => {
+    console.log("Database Connected");
+});
+
+// THE FOLLOWING FUNCTION DELETES THE DATABASE!!
+
+const seedDb = async () => {
+    await Campground.deleteMany({});
+    for(let i = 0; i < 50; i++) {
+        let random1000 = Math.floor(Math.random() * 1000);
+        let camp = new Campground({
+            title: `testLocation${i}`,
+            location: `${cities[random1000].city}, ${cities[random1000].state}`
+        })
+        await camp.save();
+    }
+}
+
+seedDb().then(() => {
+    mongoose.connection.close();
+});
