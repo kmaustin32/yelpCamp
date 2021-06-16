2a2429debeea71db6d1e205fce373fceb299051d
Keenan (Ezra) Austin <ezra@Keenans-MacBook-Pro.local>
2021-06-16 13:03:55 +0300

    required campground name

--- models/campground.js
+++ models/campground.js
@@ -2,10 +2,16 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const CampgroundSchema = new Schema({
-    title: String, 
+    title: {
+        type: String,
+        required: true
+    }, 
     price: String,
     description: String,
-    location: String
+    location: {
+        type: String,
+        required: true;
+    }
 })
 
 module.exports = mongoose.model("Campground", CampgroundSchema);
