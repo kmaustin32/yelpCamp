a4e188cabc0b433d3275e220bf11eb6bf4f3855f
Keenan (Ezra) Austin <reaper31292@gmail.com>
2021-07-19 14:16:59 +0300

    Remove Reviews Mongoose Middleware

--- app.js
+++ app.js
@@ -116,7 +116,7 @@ app.post('/campgrounds/:id/reviews', validateReview, catchAsync(async (req, res,
     res.redirect(`/campgrounds/${campground._id}`)
 }))
 
-app.delete('/campgrounds/:id/reviews/:reviewId', catchAsync( async(req, res, next) => {
+app.delete('/campgrounds/:id/reviews/:reviewId', catchAsync(async(req, res, next) => {
     const {id, reviewId} = req.params;
     await Campground.findByIdAndUpdate(id, {$pull: {reviews: reviewId}})
     await Review.findByIdAndDelete(reviewId);
