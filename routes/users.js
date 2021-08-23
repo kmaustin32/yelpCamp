4432e9ebca4f087e425238d04c16ebc3d2c917ee
Keenan (Ezra) Austin <reaper31292@gmail.com>
2021-08-23 11:52:19 +0300

    added logout and redirect after register

--- routes/users.js
+++ routes/users.js
@@ -13,8 +13,11 @@ router.post('/register', catchAsync(async (req, res) => {
         const {email, username, password} = req.body;
         const user = new User({ email, username })
         const registered = await User.register(user, password);
-        req.flash('success', 'Welcome to yelpCamp!');
-        res.redirect('/campgrounds');
+        req.login(registered, err => {
+           if(err) return next(err);
+           req.flash('success', 'Welcome to yelpCamp!');
+           res.redirect('/campgrounds');
+        })
     } 
     catch(e) {
         req.flash('error', e.message);
@@ -28,6 +31,14 @@ router.get('/login', (req, res) => {
 
 router.post('/login', passport.authenticate('local', { failureFlash: true, failureRedirect: '/login' }), (req, res) => {
     req.flash('success', 'Welcome back!');
+    const redirectUrl = req.session.returnTo || '/campgrounds'
+    delete req.session.returnTo;
+    res.redirect(redirectUrl);
+})
+
+router.get('/logout', (req, res) => {
+    req.logout();
+    req.flash('success', "You have been successfully logged out.");
     res.redirect('/campgrounds');
 })
 
