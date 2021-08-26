0a402891e725caf59a6f8d1ad780125a44b1009b
Keenan (Ezra) Austin <reaper31292@gmail.com>
2021-08-26 13:08:53 +0300

    added author field to review scheme and added isLoggedIn middleware to review post path

--- models/review.js
+++ models/review.js
@@ -9,6 +9,10 @@ const reviewSchema = new Schema({
     rating: {
         type: Number,
         requited: true
+    },
+    author: {
+        type: Schema.Types.ObjectId,
+        ref: 'User'
     }
 })
 
