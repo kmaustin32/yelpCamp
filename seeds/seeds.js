7b99b2b46ea840bf44aad398e20f5bf746447f1e
Keenan (עזרא) <reaper31292@gmail.com>
2021-09-27 10:54:42 +0300

    seed update for new laptop

--- seeds/seeds.js
+++ seeds/seeds.js
@@ -42,7 +42,7 @@ const seedDb = async () => {
             description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
             location: `${cities[random1000].city}, ${cities[random1000].state}`,
             //Taken from a user in database, change on different machine
-            author: '6127638d8bd33819b9e07a5c'
+            author: '6151776634f9691da847160d'
         })
         await camp.save();
     }
